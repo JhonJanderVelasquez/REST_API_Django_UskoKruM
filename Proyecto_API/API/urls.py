f0170d5aaad66957c119e962746c3e532970a915
JhonJanderVelasquez <jhonspeinx@gmail.com>
2022-07-29 10:15:35 -0500

    Método GET terminado.

--- Proyecto_API/API/urls.py
+++ Proyecto_API/API/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('companies/', views.CompanyView.as_view(), name="companies_list"),
+    path('companies/<int:id>', views.CompanyView.as_view(), name='companies_process'),
 ]
