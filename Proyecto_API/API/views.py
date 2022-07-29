2a4878709d1a16b32e036f70c11f684fd0bf6953
JhonJanderVelasquez <jhonspeinx@gmail.com>
2022-07-29 08:05:50 -0500

    GET funcionando en PostMan con urls

--- Proyecto_API/API/views.py
+++ Proyecto_API/API/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+from .models import Company
 
-# Create your views here.
+# Es una vista basada en una clase que hereda de vista y 
+# en url se tiene que convertir
+# en una view con class.as_view() 
+class CompanyView(View):
+
+    def get(self, request):
+        companies= list(Company.objects.values())
+        if len(companies)>0:
+            # Creación de un diccionario python si tiene datos la base de datos
+            datos={'message':"Success", 'Companies':companies}
+        else:
+            # Si no hay datos... Pues que muestre este mensaje...
+            datos={'message':"Companies not found.."}
+        return JsonResponse(datos)
+
+    def post(self, request):
+        pass
+    def put(self, request):
+        pass
+    def delete(self, request):
+        pass
